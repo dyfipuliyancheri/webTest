cc392e6137c221c52404288f0fdf1bbf4d96baf4
rahulrajsbkk <rahulrajsbkk@gmail.com>
2020-09-15 03:14:34 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
+import './static/scss/master.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/layout/MainLayout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainLayout() {
+  return <div className="mainLayout"></div>;
+}
+
+export default MainLayout;
